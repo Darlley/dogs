3551e46ba090eb5d4af3fc42e5d56f7bf53f6148
Darlley <systembrtt@gmail.com>
2024-03-01 22:56:32 -0400

    update

---
+++ src/components/menu.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <ul className='menu'>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/sobre">Sobre</Link></li>
+      <li><Link href="/contato">Contato</Link></li>
+    </ul>
+  )
+}
